32f214795c50f08a26b19c9cc6ef33f43e7ad03b
Oscar Tinajero <tinajero117.oscar@gmail.com>
2022-11-24 15:49:26 -0600

    Create OCR

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,6 +1,39 @@
 import 'package:get/get.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeController extends GetxController {
   final scanning = false.obs;
   final extractText = ''.obs;
+
+  XFile? imageFile;
+
+  Future<void> getImage(ImageSource source) async {
+    scanning.value = true;
+    try {
+      final pickedImage = await ImagePicker().pickImage(source: source);
+      if (pickedImage != null) {
+        imageFile = pickedImage;
+        await getRecognisedText(pickedImage);
+      }
+    } catch (e) {
+      extractText.value = 'Error ocurred while scanning\n\n $e';
+      imageFile = null;
+      scanning.value = false;
+    }
+  }
+
+  Future<void> getRecognisedText(XFile image) async {
+    final inputImage = InputImage.fromFilePath(image.path);
+    final textRecognizer = GoogleMlKit.vision.textRecognizer();
+    final recognisedText = await textRecognizer.processImage(inputImage);
+    await textRecognizer.close();
+    extractText.value = '';
+    for (var block in recognisedText.blocks) {
+      for (var line in block.lines) {
+        extractText.value = '${extractText.value}${line.text}\n';
+      }
+    }
+    scanning.value = false;
+  }
 }
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '/app/core/values/responsive.dart';
 import 'home_controller.dart';
@@ -20,9 +23,105 @@ class HomePage extends GetView<HomeController> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                height: 30.0.sp,
-                // decoration: BoxDecoration(),
+              Obx(
+                () => controller.scanning.value
+                    ? const CircularProgressIndicator.adaptive()
+                    : const SizedBox(),
+              ),
+              Obx(
+                () => !controller.scanning.value && controller.imageFile == null
+                    ? Container(
+                        height: 75.0.wp,
+                        width: 75.0.wp,
+                        color: Colors.grey.shade300,
+                        child: Center(
+                          child: Icon(
+                            Icons.camera_alt_outlined,
+                            size: 20.0.sp,
+                          ),
+                        ),
+                      )
+                    : controller.imageFile != null
+                        ? Image.file(
+                            File(controller.imageFile!.path),
+                            width: 75.0.wp,
+                          )
+                        : const SizedBox(),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquaredButton(
+                    iconButton: Icons.image,
+                    onPressed: () async =>
+                        await controller.getImage(ImageSource.gallery),
+                    textButton: 'Gallery',
+                  ),
+                  SquaredButton(
+                    iconButton: Icons.camera_alt,
+                    onPressed: () async =>
+                        await controller.getImage(ImageSource.camera),
+                    textButton: 'Camera',
+                  ),
+                ],
+              ),
+              SizedBox(height: 5.0.hp),
+              SizedBox(
+                child: Obx(() => Text(
+                      controller.extractText.value,
+                      style: TextStyle(fontSize: 20.0.sp),
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SquaredButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String textButton;
+  final IconData iconButton;
+  const SquaredButton({
+    super.key,
+    required this.onPressed,
+    required this.textButton,
+    required this.iconButton,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 2.0.wp),
+      padding: EdgeInsets.only(top: 3.0.hp),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.grey,
+          shadowColor: Colors.grey.shade400,
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Container(
+          margin: EdgeInsets.all(3.0.wp),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                iconButton,
+                size: 15.0.sp,
+              ),
+              Text(
+                textButton,
+                style: TextStyle(
+                  fontSize: 11.0.sp,
+                  color: Colors.grey.shade600,
+                ),
               ),
             ],
           ),
